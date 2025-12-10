973ca5016d10766db1c0e6c6e3447f22cb43340f
rayrivas950 <rayrivas950@gmail.com>
2025-12-10 16:19:42 -0500

    Borre todo para comenzar de 0, se me hizo un poco dificil avanzar asi que ahora vamos al round 2

--- app/src/main/java/com/example/audiobook/ui/theme/Theme.kt
+++
@@ -1,9 +0,0 @@
-package com.example.audiobook.ui.theme
-
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-
-@Composable
-fun AudiobookAppTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
-}
